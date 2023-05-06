6778259b7880198180f1290556afc2c352507ef0
ReaJason <reajason1225@gmail.com>
2023-05-06 13:04:17 +0800

    feat: add ip block error exception

--- xhs/exception.py
+++ xhs/exception.py
@@ -2,4 +2,8 @@ from requests import RequestException
 
 
 class DataFetchError(RequestException):
-    """"""
+    """something error when fetch"""
+
+
+class IPBlockError(RequestException):
+    """fetch so fast that the server block us ip"""
