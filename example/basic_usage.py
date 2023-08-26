a448fe22ccb8ad0e89306bff6038401ac95fa353
ReaJason <reajason1225@gmail.com>
2023-08-26 11:52:28 +0800

    fix: sign error, resolved #40,#43

--- example/basic_usage.py
+++ example/basic_usage.py
@@ -1,50 +1,52 @@
+import datetime
+import json
 from time import sleep
-from xhs import XhsClient
+from xhs import XhsClient, DataFetchError
 from playwright.sync_api import sync_playwright
 
 
-def get_context_page(instance, stealth_js_path):
-    chromium = instance.chromium
-    browser = chromium.launch(headless=True)
-    context = browser.new_context(
-        viewport={"width": 1920, "height": 1080},
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 "
-                   "Safari/537.36"
-    )
-    context.add_init_script(path=stealth_js_path)
-    page = context.new_page()
-    return context, page
-
-
 def sign(uri, data=None, a1="", web_session=""):
-    context_page.goto("https://www.xiaohongshu.com")
-    cookie_list = browser_context.cookies()
-    web_session_cookie = list(filter(lambda cookie: cookie["name"] == "web_session", cookie_list))
-    if not web_session_cookie:
-        browser_context.add_cookies([
-            {'name': 'web_session', 'value': web_session, 'domain': ".xiaohongshu.com", 'path': "/"},
-            {'name': 'a1', 'value': a1, 'domain': ".xiaohongshu.com", 'path': "/"}]
-        )
-        sleep(1)
-    encrypt_params = context_page.evaluate("([url, data]) => window._webmsxyw(url, data)", [uri, data])
-    return {
-        "x-s": encrypt_params["X-s"],
-        "x-t": str(encrypt_params["X-t"])
-    }
+    for _ in range(10):
+        try:
+            with sync_playwright() as playwright:
+                stealth_js_path = "/Users/reajason/ReaJason/xhs/tests/stealth.min.js"
+                chromium = playwright.chromium
+
+                # 如果一直失败可尝试设置成 False 让其打开浏览器，适当添加 sleep 可查看浏览器状态
+                browser = chromium.launch(headless=True)
+
+                browser_context = browser.new_context()
+                browser_context.add_init_script(path=stealth_js_path)
+                context_page = browser_context.new_page()
+                context_page.goto("https://www.xiaohongshu.com")
+                browser_context.add_cookies([
+                    {'name': 'web_session', 'value': web_session, 'domain': ".xiaohongshu.com", 'path': "/"},
+                    {'name': 'a1', 'value': a1, 'domain': ".xiaohongshu.com", 'path': "/"}]
+                )
+
+                # 这个地方设置完浏览器 cookie 之后，如果这儿不 sleep 一下签名获取就失败了，如果经常失败请设置长一点试试
+                sleep(1)
+                encrypt_params = context_page.evaluate("([url, data]) => window._webmsxyw(url, data)", [uri, data])
+                return {
+                    "x-s": encrypt_params["X-s"],
+                    "x-t": str(encrypt_params["X-t"])
+                }
+        except Exception:
+            # 这儿有时会出现 window._webmsxyw is not a function 或未知跳转错误，因此加一个失败重试趴
+            pass
+    raise Exception("重试了这么多次还是无法签名成功，寄寄寄")
 
 
 if __name__ == '__main__':
     cookie = "please get cookie from your website"
-    stealth_js_path = "/Users/reajason/ReaJason/xhs/tests/stealth.min.js"
-    playwright = sync_playwright().start()
-    browser_context, context_page = get_context_page(playwright, stealth_js_path)
 
     xhs_client = XhsClient(cookie, sign=sign)
+    print(datetime.datetime.now())
 
-    # get note info
-    note_info = xhs_client.get_note_by_id("63db8819000000001a01ead1")
-
-    print(note_info)
-
-    # resource release
-    playwright.stop()
+    for _ in range(10):
+        # 即便上面做了重试，还是有可能会遇到签名失败的情况，重试即可
+        try:
+            print(json.dumps(xhs_client.get_self_info()["basic_info"], indent=4))
+            break
+        except DataFetchError as e:
+            print("失败重试一下下")
